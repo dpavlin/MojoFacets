d3e7cd2539c61bc88d22442dc6730cd4df770480
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-04-01 16:22:29 +0200

    use JSON which is much faster than Mojo::JSON

--- lib/MojoFacets/Data.pm
+++ lib/MojoFacets/Data.pm
@@ -1,17 +1,21 @@
-package MojoFacets::Example;
+package MojoFacets::Data;
 
 use strict;
 use warnings;
 
 use base 'Mojolicious::Controller';
 
+use Data::Dump qw(dump);
+use File::Slurp;
+use JSON;
+
 our $data;
 
 sub _data {
 	my $self = shift;
-	my $json = Mojo::JSON->new;
 
-	$data ||= $json->decode(  File::Spec->catfile( $self->root, 'data', 'bibpsi.js' ) );
+	# we could use Mojo::JSON here, but it's too slow
+	$data ||= from_json read_file $self->app->home->rel_file( 'data/bibpsi.js' );
 }
 
 
@@ -20,13 +24,16 @@ sub stats {
 
 	$self->_data;
 
+	my $stats;
+
 	foreach my $e ( @{ $data->{items} } ) {
 		foreach my $n ( keys %$e ) {
 			$stats->{column}->{$n}->{count}++;
 			$stats->{column}->{$n}->{number}++ if $e->{$n} =~ m/[-+]?([0-9]*\.[0-9]+|[0-9]+)/;
 		}
 	}
-	warn "# stats ", $self->Dumper( $stats );
+
+	$self->app->log->debug( 'stats', dump($stats) );
     # Render template "example/welcome.html.ep" with message
     $self->render(
 		message => 'Welcome to the Mojolicious Web Framework!',
