706a177e3ee0ecb7501eb017d82e2b3943c3331c
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-05-22 14:31:08 +0200

    display all changes on disk

--- lib/MojoFacets/Changes.pm
+++ lib/MojoFacets/Changes.pm
@@ -8,8 +8,17 @@ use base 'Mojolicious::Controller';
 sub index {
 	my $self = shift;
 
+	my $changes;
+	foreach my $path ( glob '/tmp/changes/*' ) {
+		if ( $path =~ m{/(\d+\.\d+)\.(.+)$} ) {
+			push @$changes, [ $1, split(/\./, $2) ];
+		} else {
+			warn "ignore: $path\n";
+		}
+	}
+
 	# Render template "changes/index.html.ep" with message
-	$self->render(message => 'Latest Changes');
+	$self->render(message => 'Latest Changes', changes => $changes );
 }
 
 1;
