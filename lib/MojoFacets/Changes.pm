31d07c4ea780f9d87f6b04325a625eb732ed3332
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-05-23 18:09:59 +0200

    show 10 last changes

--- lib/MojoFacets/Changes.pm
+++ lib/MojoFacets/Changes.pm
@@ -10,10 +10,13 @@ use Storable;
 sub index {
 	my $self = shift;
 
+	my $max = $self->param('max') || 10;
+
 	my $changes;
-	foreach my $path ( glob '/tmp/changes/*' ) {
+	foreach my $path ( sort { $b cmp $a } glob '/tmp/changes/*' ) {
 		if ( $path =~ m{/((\d+\.\d+)\.data\.(.+))$} ) {
 			push @$changes, { uid => $1, t => $2, action => $3 };
+			last if $#$changes > $max;
 		} else {
 			warn "ignore: $path\n";
 		}
