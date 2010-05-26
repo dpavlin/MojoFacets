2d6dc8d82ca0e1afdd1055a7b5ebd580fe625857
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-05-26 14:18:41 +0200

    added repetable column

--- data/test.js
+++ data/test.js
@@ -2,16 +2,20 @@
  "items":[{
 	  "id":[0]
 	, "alpha":["a"]
+	, "repeatable":[1,2,3]
 	},{
 	  "id":[1]
 	, "alpha":["c"]
+	, "repeatable":[4,5,6]
 	},{
 	  "id":[2]
 	, "alpha":["b"]
+	, "repeatable":[7,8,9]
 	},{
 	  "id":[42]
 	, "alpha":["b"]
+	, "repeatable":[1,3,5,7,9]
 	}
 ],
- "header":["id","alpha"]
+ "header":["id","alpha","repeatable"]
 }
