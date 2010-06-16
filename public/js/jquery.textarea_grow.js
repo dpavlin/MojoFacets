993c283e9a1a564b1882547fd91cfbf01557ca6a
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-16 20:46:37 +0200

    textarea_grow jquery plugin

---
+++ public/js/jquery.textarea_grow.js
@@ -0,0 +1,16 @@
+jQuery.fn.textarea_grow = function(){
+	return this.each(function(){
+		var rows = this.rows;
+		console.debug( 'textarea_grow', rows, this );
+		var grow = function(ta) {
+    		var lines = ta.value.split('\n').length;
+			if ( lines > rows ) {
+				ta.rows = lines;
+				//console.debug('keyup', lines, rows, ta );
+			}
+		};
+		grow(this);
+		this.onkeyup = function() { grow(this) };
+	});
+};
+
