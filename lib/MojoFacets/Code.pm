01daa04c8b57d1debaffff8659f94b57c77a5313
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-19 15:30:02 +0200

    show all applicable code snippets
    This required us to check if current dataset (using columns) has all
    applicable columns for given dataset

--- lib/MojoFacets/Code.pm
+++ lib/MojoFacets/Code.pm
@@ -12,6 +12,7 @@ sub index {
 	my $self = shift;
 
 	$self->redirect_to('/data/columns') unless $self->session('columns');
+	my $columns = { map { $_ => 1 } @{ $self->session('columns') } };
 
 	my $dir = $self->app->home->rel_dir('public') . '/code';
 
@@ -21,8 +22,15 @@ sub index {
 		my $path = $full_path;
 		$path =~ s/^$dir\/*//;
 		$path =~ s/\.pl$//;
-		my ( $column, $description ) = split(/\./,$path,2);
-		$snippets->{$column}->{$description} = read_file $full_path;
+		my ( $depends, $description ) = split(/\./,$path,2);
+
+		my @deps = split(/,/,$depends);
+		my $found = -1;
+		$found += $columns->{$_} foreach @deps;
+warn "# depends $depends $found $#deps\n";
+		next unless $found == $#deps;
+
+		$snippets->{$depends}->{$description} = read_file $full_path;
 	}
 
 	$self->render(
