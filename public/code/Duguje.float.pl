3a01abbcbf42145fe460aa6310b3b36337173460
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-22 16:26:29 +0200

    reformat float using sprintf

--- public/code/Duguje.float.pl
+++ public/code/Duguje.float.pl
@@ -2,7 +2,6 @@ foreach my $col ( grep { m/(Duguje|Potra.uje|Saldo)/ } keys %$row ) {
  foreach my $v ( @{ $row->{$col} } ) {
   push @{ $update->{ $col . '_old' } }, $v;
   $v =~ s/(\d+)?\.?(\d{0,3})\,(\d{2})/$1$2.$3/;
-  $v *= 1;
-  push @{ $update->{$col} }, $v;
+  push @{ $update->{$col} }, sprintf('%.2f',$v);
  }
 }
