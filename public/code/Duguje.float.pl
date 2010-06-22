dd9984eccb5f07a34689992cd0d353b94da2089c
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-22 16:14:52 +0200

    modify multiple columns

--- public/code/Duguje.float.pl
+++ public/code/Duguje.float.pl
@@ -1,6 +1,8 @@
-foreach my $v ( @{ $row->{Duguje} } ) {
- push @{ $update->{Duguje_old} }, $v;
- $v =~ s/(\d+)?\.?(\d{0,3})\,(\d{2})/$1$2.$3/;
- $v *= 1;
- push @{ $update->{Duguje} }, $v;
+foreach my $col ( grep { m/(Duguje|Potra.uje|Saldo)/ } keys %$row ) {
+ foreach my $v ( @{ $row->{$col} } ) {
+  push @{ $update->{ $col . '_old' } }, $v;
+  $v =~ s/(\d+)?\.?(\d{0,3})\,(\d{2})/$1$2.$3/;
+  $v *= 1;
+  push @{ $update->{$col} }, $v;
+ }
 }
