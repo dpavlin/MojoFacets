53588a28617774a7323799597186209704068dec
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-22 17:01:47 +0200

    don't generate zero for null values

--- public/code/Duguje.float.pl
+++ public/code/Duguje.float.pl
@@ -1,5 +1,6 @@
 foreach my $col ( grep { m/(Duguje|Potra.uje|Saldo)/ } keys %$row ) {
  foreach my $v ( @{ $row->{$col} } ) {
+  next unless length($v) > 0;
   push @{ $update->{ $col . '_old' } }, $v;
   $v =~ s/(\d+)?\.?(\d{0,3})\,(\d{2})/$1$2.$3/;
   push @{ $update->{$col} }, sprintf('%.2f',$v);
