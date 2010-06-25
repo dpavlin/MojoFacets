778f455353e687a5911c740cd03b90146ec1cb42
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-25 18:51:42 +0200

    remove CR from code snippets

--- lib/MojoFacets/Data.pm
+++ lib/MojoFacets/Data.pm
@@ -647,7 +647,8 @@ sub items {
 	my $data = $self->_loaded('data');
 
 	my $code = $self->_param_scalar('code','');
-	$code =~ s{[\r\n]+$}{\n}s;
+	$code =~ s{\r}{}gs;
+	$code =~ s{\n+$}{\n}s;
 
 	my $commit = $self->param('commit');
 	my $test = $self->param('test');
