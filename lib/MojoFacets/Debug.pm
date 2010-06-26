fe64bd6e02ea14ef7d30bc4d6e777ba2775376f9
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-06-26 18:41:06 +0200

    display size of data using Storable
    Discussion about this is at:
    
    http://www.perlmonks.org/?node_id=846678
    
    http://gist.github.com/454137

--- lib/MojoFacets/Debug.pm
+++ lib/MojoFacets/Debug.pm
@@ -6,14 +6,22 @@ use warnings;
 use base 'Mojolicious::Controller';
 
 use Data::Dump qw(dump);
+use Storable;
 
 sub index {
 	my $self = shift;
 
-
 	$self->render(
 		loaded => $MojoFacets::Data::loaded,
 	);
+}
+
+sub _ref_size {
+	my ( $self, $ref ) = @_;
+	return unless ref($ref);
+	open(my $fh, '|-', 'cat > /dev/null');
+	Storable::store_fd $ref, $fh;
+	tell($fh);
 }
 
 1
