9417dd88ec6cd188b2287f8f9d20e5b4c20d693a
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-09-22 10:04:43 +0200

    support mirror pools

--- public/code/pool.label.pl
+++ public/code/pool.label.pl
@@ -1,6 +1,6 @@
 foreach my $v ( @{ $row->{'pool'} } ) {
  my $drives = $v =~ s/sd/sd/g;
- my $vdevs  = $v =~ s/raidz/raidz/g;
+ my $vdevs  = $v =~ s/(raidz|mirror)/$1/g;
  my $c = $drives / $vdevs;
  $update->{label} = "$vdevs.$c";
 }
