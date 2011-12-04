d1bad7313f161d0c14efcc64621f1f66ac2d1e02
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-12-04 18:55:08 +0100

    we need additional perl module

--- bin/debian-install.sh
+++ bin/debian-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 
-sudo apt-get install libhtml-tableextract-perl libjson-perl libmodule-install-perl libstatistics-descriptive-perl libmodule-install-perl
+sudo apt-get install libhtml-tableextract-perl libjson-perl libmodule-install-perl libstatistics-descriptive-perl libmodule-install-perl libtext-unaccent-pureperl-perl
