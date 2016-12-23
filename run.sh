8787dd45e516ac842130fd9fb3d083ce2201d5c7
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-23 14:25:06 +0100

    expand mojo inactivity timeout

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 
-LANG=hr_HR.utf8 morbo ./script/mojo_facets 2>&1 | tee /tmp/mojo_facets.log
+MOJO_INACTIVITY_TIMEOUT=99999 LANG=hr_HR.utf8 morbo ./script/mojo_facets 2>&1 | tee /tmp/mojo_facets.log
