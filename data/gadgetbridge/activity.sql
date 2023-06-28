cc40ec279fa56113bbae89a3a14f63f864ea1b4b
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-06-28 11:40:02 +0200

    extract date only for facet selection of date range

--- data/gadgetbridge/activity.sql
+++ data/gadgetbridge/activity.sql
@@ -1,7 +1,11 @@
 -- dbi:SQLite:dbname=/srv/mojo_facets/data/gadgetbridge/Gadgetbridge
 -- timefmt %Y-%m-%d %H:%M:%S
 
-SELECT "TIMESTAMP",datetime("TIMESTAMP",'unixepoch','localtime') as DATETIME, STEPS,
+SELECT
+-- "TIMESTAMP",
+date(datetime("TIMESTAMP",'unixepoch','localtime')) as DATE,
+datetime("TIMESTAMP",'unixepoch','localtime') as DATETIME,
+STEPS,
 CASE
 WHEN HEART_RATE = 255 THEN NULL 
 --WHEN HEART_RATE < 2   THEN 'B' 
@@ -10,9 +14,9 @@ as HEART_RATE,
 RAW_INTENSITY,
 RAW_KIND
 FROM MI_BAND_ACTIVITY_SAMPLE
-WHERE "TIMESTAMP" > unixepoch() - ( 24 * 60 * 60 * 7 ) 
+WHERE RAW_INTENSITY != -1
+-- and "TIMESTAMP" > unixepoch() - ( 24 * 60 * 60 * 3 ) 
 
---WHERE "TIMESTAMP" BETWEEN (strftime('%s','2019-08-02 16:15:00','utc')) and (strftime('%s','2099-08-03 23:15:00','utc'))
---and HEART_RATE<>255
-and RAW_INTENSITY != -1
+-- and "TIMESTAMP" BETWEEN (strftime('%s','2019-08-02 16:15:00','utc')) and (strftime('%s','2099-08-03 23:15:00','utc'))
+-- and HEART_RATE<>255
 ;
